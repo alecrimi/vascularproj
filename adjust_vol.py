795c212634aee480f0dce70412f6fd568cdc267c
Alessandro Crimi <alecrimi@alice.it>
2020-04-07 17:46:39 +0200

    Update adjust_vol.py

--- adjust_vol.py
+++ adjust_vol.py
@@ -1,47 +1,16 @@
 # CLAHE local contrast adjustment for multipage tif volumes
 
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
-
 from skimage import img_as_float
-from skimage import exposure,io
-from tifffile import imsave #This has to be fixed more friendly directly with Skimage
+from skimage import exposure,io 
+from skimage import external
+
+#Currently saving at 32 bits, we need it at 16 bits
 
 # Load an example image
-namefile = 'slice.png'
+input_namefile = 'slice.png'
+output_namefile = 'adjusted.tif'
 
-#matplotlib.rcParams['font.size'] = 8
-'''
-def plot_img_and_hist(image, axes, bins=256):
-    """Plot an image along with its histogram and cumulative histogram.
-
-    """
-    image = img_as_float(image)
-    ax_img, ax_hist = axes
-    ax_cdf = ax_hist.twinx()
-
-    # Display image
-    ax_img.imshow(image, cmap=plt.cm.gray)
-    ax_img.set_axis_off()
-
-    # Display histogram
-    ax_hist.hist(image.ravel(), bins=bins, histtype='step', color='black')
-    ax_hist.ticklabel_format(axis='y', style='scientific', scilimits=(0, 0))
-    ax_hist.set_xlabel('Pixel intensity')
-    ax_hist.set_xlim(0, 1)
-    ax_hist.set_yticks([])
-
-    # Display cumulative distribution
-    img_cdf, bins = exposure.cumulative_distribution(image, bins)
-    ax_cdf.plot(bins, img_cdf, 'r')
-    ax_cdf.set_yticks([])
-
-    return ax_img, ax_hist, ax_cdf
-'''
-
-img = io.imread(namefile)
-#img = data.moon()
+img = io.imread(namefile) 
 '''
 # Contrast stretching
 p2, p98 = np.percentile(img, (2, 98))
@@ -54,4 +23,4 @@ img_eq = exposure.equalize_hist(img)
 img_adapteq = exposure.equalize_adapthist(img, clip_limit=0.03)
 #img_adapteq = img_adapteq.astype(np.uint16)
 #io.imsave('result.tif',img_adapteq)
-io.imsave(fname="chair.tiff", arr=img_adapteq,plugin="tifffile")
+external.tifffile.imsave(output_namefile, img_adapteq,software='Imagej') #,plugin="tifffile"
