648d48609f3cb5f7712a2bc136325fb9e332c52d
Alessandro Crimi <alecrimi@alice.it>
2020-04-07 18:44:40 +0200

    Update adjust_vol.py

--- adjust_vol.py
+++ adjust_vol.py
@@ -2,25 +2,18 @@
 
 from skimage import img_as_float
 from skimage import exposure,io 
-from skimage import external
-
-#Currently saving at 32 bits, we need it at 16 bits
+from skimage import external 
+from skimage.color import rgb2gray
+import numpy as np
 
 # Load an example image
 input_namefile = 'slice.png'
 output_namefile = 'adjusted.tif'
 
-img = io.imread(namefile) 
-'''
-# Contrast stretching
-p2, p98 = np.percentile(img, (2, 98))
-img_rescale = exposure.rescale_intensity(img, in_range=(p2, p98))
-
-# Equalization
-img_eq = exposure.equalize_hist(img)
-'''
+img = io.imread(input_namefile) 
+ 
 # Adaptive Equalization
 img_adapteq = exposure.equalize_adapthist(img, clip_limit=0.03)
-#img_adapteq = img_adapteq.astype(np.uint16)
-#io.imsave('result.tif',img_adapteq)
-external.tifffile.imsave(output_namefile, img_adapteq,software='Imagej') #,plugin="tifffile"
+img_adapteq = img_adapteq.astype(np.float16)
+img_gray = rgb2gray(img_adapteq) 
+external.tifffile.imsave(output_namefile, img_gray,software='Imagej',bigtiff=True) #,plugin="tifffile"
