93f92ab50c2f587288a5b3c138e8ee2ec4fc344b
Alessandro Crimi <alecrimi@alice.it>
2020-04-07 17:12:40 +0200

    Update adjust_vol.py

--- adjust_vol.py
+++ adjust_vol.py
@@ -1,18 +1,57 @@
 # CLAHE local contrast adjustment for multipage tif volumes
 
-from skimage import data,io,exposure,img_as_float
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 
-############################ I AM CURRENTLY CHANGING THIS, TRY THE OTHER SCRIPT ###############
+from skimage import img_as_float
+from skimage import exposure,io
+from tifffile import imsave
 
-namefile = 'pvalues_p0.0001_montage_mip_alt2.tif'
+# Load an example image
+namefile = 'slice.png'
 
-im = io.imread(namefile)
-#im = data.moon()
-print(im.shape)
-im = img_as_float(im)
-im_s = exposure.rescale_intensity(im, in_range=(-1, 1)) 
-#Clipping limit, normalized between 0 and 1 (higher values give more contrast).
-img_adapteq = exposure.equalize_adapthist(im_s, clip_limit=0.03)
-img_adapteq = img_adapteq.astype(np.uint16)
-io.imsave('result.tif',img_adapteq)
+#matplotlib.rcParams['font.size'] = 8
+'''
+def plot_img_and_hist(image, axes, bins=256):
+    """Plot an image along with its histogram and cumulative histogram.
+
+    """
+    image = img_as_float(image)
+    ax_img, ax_hist = axes
+    ax_cdf = ax_hist.twinx()
+
+    # Display image
+    ax_img.imshow(image, cmap=plt.cm.gray)
+    ax_img.set_axis_off()
+
+    # Display histogram
+    ax_hist.hist(image.ravel(), bins=bins, histtype='step', color='black')
+    ax_hist.ticklabel_format(axis='y', style='scientific', scilimits=(0, 0))
+    ax_hist.set_xlabel('Pixel intensity')
+    ax_hist.set_xlim(0, 1)
+    ax_hist.set_yticks([])
+
+    # Display cumulative distribution
+    img_cdf, bins = exposure.cumulative_distribution(image, bins)
+    ax_cdf.plot(bins, img_cdf, 'r')
+    ax_cdf.set_yticks([])
+
+    return ax_img, ax_hist, ax_cdf
+'''
+
+img = io.imread(namefile)
+#img = data.moon()
+'''
+# Contrast stretching
+p2, p98 = np.percentile(img, (2, 98))
+img_rescale = exposure.rescale_intensity(img, in_range=(p2, p98))
+
+# Equalization
+img_eq = exposure.equalize_hist(img)
+'''
+# Adaptive Equalization
+img_adapteq = exposure.equalize_adapthist(img, clip_limit=0.03)
+#img_adapteq = img_adapteq.astype(np.uint16)
+#io.imsave('result.tif',img_adapteq)
+io.imsave(fname="chair.tiff", arr=img_adapteq,plugin="tifffile")
